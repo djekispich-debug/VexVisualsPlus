98b2bd1964285e308513f341797f0aaa53ec0715
djekispich-debug <djekispich@gmail.com>
2026-05-31 21:30:43 +0300

    Update ThemeManager.java

--- src/main/java/VexVisuals/gui/ThemeManager.java
+++ src/main/java/VexVisuals/gui/ThemeManager.java
@@ -1,82 +1,28 @@
-package VexVisuals.gui;
+package com.yourmod.gui.theme;
 
-public final class ThemeManager {
-    private static GUITheme current = GUITheme.DARK;
+import java.util.ArrayList;
+import java.util.List;
 
-    private ThemeManager() {
+public class ThemeManager {
+    private static final List<GUITheme> themes = new ArrayList<>();
+    private static int currentIndex = 0;
+
+    static {
+        themes.add(GUITheme.DARK);
+        themes.add(GUITheme.PURPLE);
+        themes.add(GUITheme.RED);
+        // Добавляй свои темы сюда
     }
 
-    public static GUITheme getCurrent() {
-        return current;
+    public static GUITheme getCurrentTheme() {
+        return themes.get(currentIndex);
     }
 
-    public static void setTheme(GUITheme theme) {
-        current = theme;
+    public static void nextTheme() {
+        currentIndex = (currentIndex + 1) % themes.size();
     }
 
-    public static void cycleTheme() {
-        GUITheme[] values = GUITheme.values();
-        current = values[(current.ordinal() + 1) % values.length];
-    }
-
-    public static int background() {
-        return switch (current) {
-            case LIGHT -> 0xE8F0F8FF;
-            case CYBERPUNK -> 0xD0101828;
-            case SAKURA -> 0xD0281A24;
-            default -> 0xD0121218;
-        };
-    }
-
-    public static int panel() {
-        return switch (current) {
-            case LIGHT -> 0xF5FFFFFF;
-            case CYBERPUNK -> 0xF0182030;
-            case SAKURA -> 0xF032242C;
-            default -> 0xF01A1A22;
-        };
-    }
-
-    public static int text() {
-        return switch (current) {
-            case LIGHT -> 0xFF1A1A24;
-            case CYBERPUNK -> 0xFFE0FFFF;
-            case SAKURA -> 0xFFFFF0F5;
-            default -> 0xFFF2F2F7;
-        };
-    }
-
-    public static int textMuted() {
-        return switch (current) {
-            case LIGHT -> 0xFF6B7280;
-            case CYBERPUNK -> 0xFF7DD3FC;
-            case SAKURA -> 0xFFF9A8D4;
-            default -> 0xFF9CA3AF;
-        };
-    }
-
-    public static int accent() {
-        return switch (current) {
-            case LIGHT -> 0xFF2563EB;
-            case CYBERPUNK -> 0xFF00F5FF;
-            case SAKURA -> 0xFFFF6B9D;
-            default -> 0xFF8B5CF6;
-        };
-    }
-
-    public static int border() {
-        return withAlpha(accent(), 0x80);
-    }
-
-    public static int headerGradientTop() {
-        return withAlpha(accent(), 0x55);
-    }
-
-    public static int headerGradientBottom() {
-        return withAlpha(background(), 0xFF);
-    }
-
-    private static int withAlpha(int color, int alpha) {
-        return (color & 0x00FFFFFF) | (alpha << 24);
+    public static List<GUITheme> getAllThemes() {
+        return themes;
     }
 }
