a3af2bb60e554ac1f912fa85d96d4aba6ffe09fc
djekispich-debug <djekispich@gmail.com>
2026-06-01 12:42:12 +0300

    Update Category.java

--- src/main/java/VexVisuals/module/Category.java
+++ src/main/java/VexVisuals/module/Category.java
@@ -1,12 +1,20 @@
 package VexVisuals.module;
 
 public enum Category {
+    // Основные категории для ClickGui
     COMBAT("Combat"),
     RENDER("Render"),
     MOVEMENT("Movement"),
     PLAYER("Player"),
     MISC("Misc"),
-    MUSIC("Music");
+    MUSIC("Music"),
+
+    // Дополнительные категории для ModuleType
+    HUD("HUD"),
+    COMBAT_VISUALS("Combat Visuals"),
+    INDICATORS("Indicators"),
+    COSMETICS("Cosmetics"),
+    WORLD_STYLE("World Style");
 
     private final String name;
 
